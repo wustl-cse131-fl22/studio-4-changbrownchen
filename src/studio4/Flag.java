7253f4f0d81377fb2536091957b86d5857d19556
meaganchang <meaganchang@meagans-air.dhcp.wustl.edu>
2022-10-12 15:32:14 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -2,10 +2,24 @@ package studio4;
 
 import java.awt.Color;
 
+import javax.swing.SwingUtilities;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setXscale(0, 20);
+		StdDraw.setYscale(0, 10);
+		StdDraw.setPenColor(255, 0, 255);
+		StdDraw.filledRectangle(10, 5, 9, 4);
+		StdDraw.setPenColor(0, 200, 200);
+		StdDraw.filledEllipse(10, 5, 6, 4);;
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setPenColor(255, 0, 255);
+		StdDraw.filledSquare(10, 5, 1);
+						}
+
 		
-	}
-}
+
+				}
+			
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -9,9 +10,7 @@ import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
-/**
- * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
- */
+
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -19,6 +18,8 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		System.out.print("Choose your shape (write in all lower case)");
+		String shape = in.next(); 
 		
 	}
 }
